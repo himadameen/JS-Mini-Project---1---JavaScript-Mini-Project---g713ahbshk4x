7beac0ddf0f4db33bd4ea2fa37d6b586e59d7945
himmad ameen <ameenhh1620@gmail.com>
2022-06-08 23:31:41 +0530

    my changes

--- App.js
+++ App.js
@@ -2,82 +2,88 @@
 var startButton = document.querySelector('#play');
 var gameStatus = "on";
 
-startButton.addEventListener('click',()=>{
+startButton.addEventListener('click', () => {
     if (gameStatus == "on") {
         playGame();
-        document.querySelector('#yourscore').textContent='0';
-        document.querySelector('#dur_game').disabled=true;
-        document.querySelector('#game_speed').disabled=true;
-        document.querySelector('#game_speed').style.cursor='auto';
-        document.querySelector('.duration').style.cursor='auto';
-        document.querySelector('#dur_game').style.cursor='auto';
-        gameStatus='Running';
-        startButton.style.background='#2980b9';     
-        document.querySelector('#play_txt').textContent='Game is ON!!'; 
-    } 
-       
+        document.querySelector('#yourscore').textContent = '0';
+        document.querySelector('#dur_game').disabled = true;
+        document.querySelector('#game_speed').disabled = true;
+        document.querySelector('#game_speed').style.cursor = 'auto';
+        document.querySelector('.duration').style.cursor = 'auto';
+        document.querySelector('#dur_game').style.cursor = 'auto';
+        gameStatus = 'Running';
+        startButton.style.background = '#2980b9';
+        document.querySelector('#play_txt').textContent = 'Game is ON!!';
+    }
+
 })
 
-function playGame(){
+function playGame() {
     var grid = document.querySelectorAll('.block');
     var hitPosition;
-    var score = 0; 
+    var score = 0;
     var gameSpeed = 750;
 
     var timer_html = document.querySelector('#timer');
     var timeLeft = document.querySelector('#dur_game').value;
     gameSpeed = document.querySelector('#game_speed').value;
 
-var timerID = setInterval(function(){
-    if (timeLeft<1) {
-        clearInterval(timerID);
-        gameStatus="on";
-        document.querySelector('#play_txt').textContent='Play Again';
-        startButton.style.background='#27ae60';    
-        startButton.style.fontWeight='Initial'; 
-        document.querySelector('#dur_game').disabled=false;
-        document.querySelector('#game_speed').disabled=false;
-        document.querySelector('#game_speed').style.cursor='pointer';
-        document.querySelector('.duration').style.cursor='pointer';
-        document.querySelector('#dur_game').style.cursor='pointer';
-    }
-
-    timer_html.textContent=timeLeft;
-    timeLeft--;
- }, 1000);
-
-function popJerry(randomBLockNo){
-    
-    grid[randomBLockNo].classList.add("jerry");
-    grid[randomBLockNo].classList.add("jerry2");
-}
-
-function removeJerry(){
-    grid.forEach(element => {
-        element.classList.remove("jerry");
-        element.classList.remove("jerry2");
-    });
-}
-
-grid.forEach(element => {
-    element.addEventListener('click',()=>{
-        if(element.id == hitPosition+1 && timeLeft > 0){
-            score++;
-            document.querySelector('#yourscore').textContent = score;            
+    var timerID = setInterval(function () {
+        if (timeLeft < 1) {
+            clearInterval(timerID);
+            gameStatus = "on";
+            document.querySelector('#play_txt').textContent = 'Play Again';
+            startButton.style.background = '#27ae60';
+            startButton.style.fontWeight = 'Initial';
+            document.querySelector('#dur_game').disabled = false;
+            document.querySelector('#game_speed').disabled = false;
+            document.querySelector('#game_speed').style.cursor = 'pointer';
+            document.querySelector('.duration').style.cursor = 'pointer';
+            document.querySelector('#dur_game').style.cursor = 'pointer';
         }
-    })
-});
 
-var jerryTimer = setInterval(() => {
-    if (timeLeft < 1) {
-        clearInterval(jerryTimer)
+        timer_html.textContent = timeLeft;
+        timeLeft--;
+
+        // document.body.background = "grey";    
+        if (timeLeft == 0){
+            alert("your score is " + score);
+        }
+    }, 1000);
+
+    function popJerry(randomBLockNo) {
+
+        grid[randomBLockNo].classList.add("jerry");
+        grid[randomBLockNo].classList.add("jerry2");
     }
-    removeJerry();
-    let randomBLockNo = Math.floor((Math.random())*16);
-    hitPosition = randomBLockNo;
-    popJerry(randomBLockNo);
-}, gameSpeed);
 
+    function removeJerry() {
+        grid.forEach(element => {
+            element.classList.remove("jerry");
+            element.classList.remove("jerry2");
+        });
+    }
+
+    grid.forEach(element => {
+        element.addEventListener('click', () => {
+            if (element.id == hitPosition + 1 && timeLeft > 0) {
+                score++;
+                document.querySelector('#yourscore').textContent = score;
+            }
+        })
+    });
+
+    var jerryTimer = setInterval(() => {
+        if (timeLeft < 1) {
+            clearInterval(jerryTimer)
+        }
+        removeJerry();
+        let randomBLockNo = Math.floor((Math.random()) * 16);
+        hitPosition = randomBLockNo;
+        popJerry(randomBLockNo);
+        
+    }, gameSpeed);
+    
 }
 
 
